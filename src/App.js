e9d930af4c9012a9b9d8fba00b45b1401f34b024
kgittens89 <93796800+kgittens89@users.noreply.github.com>
2022-02-15 09:53:09 -0600

    Add React router w/ route setup
    Co-Authored-By: rayhaynes504 <93682733+rayhaynes504@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import About from './components/About/About';
+import BookDetail from './components/BookDetail/BookDetail';
+import Booklist from './components/Booklist/Booklist';
+import Login from './components/Login/Login';
+import NewBook from './components/NewBook/NewBook';
 
 function App() {
   return (
-    <div className="App">
-      App
+    <div className='App'>
+      <Navigation />
+      <Routes>
+        <Route path='/about' element={<About />} />
+        <Route path='/book/:id' element={<BookDetail />} />
+        <Route path='/books' element={<Booklist />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/new-book' element={<NewBook />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
