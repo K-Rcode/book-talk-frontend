a53ac05a36bd56af6b24809580af1f43c6670b29
kgittens89 <93796800+kgittens89@users.noreply.github.com>
2022-02-15 13:32:51 -0600

    Add api config, new book form

--- src/components/NewBook/NewBook.jsx
+++ src/components/NewBook/NewBook.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Button from 'react-bootstrap/Button'
 
 function NewBook(props) {
+    
+    const handleChange = () => {
+        console.log('hi')
+    }
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('submit')
+    }
+    
     return (
-        <div>
-            New Book
+        <div className='m-5 w-75 '>
+        <Form onSubmit={handleSubmit}>
+        <FloatingLabel
+        controlId="floatingTitle"
+        label="Title"
+        className="mb-3"
+        >
+        <Form.Control placeholder="Title" onChange={ handleChange} />
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingAuthor" label="Author">
+        <Form.Control placeholder="Author" />
+        </FloatingLabel>
+        <Button variant="secondary" type="submit" className='m-3'>
+        Submit
+        </Button>
+        </Form>
         </div>
-    );
-}
-
-export default NewBook;
+        );
+    }
+    
+    export default NewBook;
