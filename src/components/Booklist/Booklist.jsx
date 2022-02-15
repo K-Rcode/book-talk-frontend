e3d4db664cfc53e01fae2a2b87dbda3e3868b37e
kgittens89 <93796800+kgittens89@users.noreply.github.com>
2022-02-15 09:27:27 -0600

    Component Setup, Add API key for Google Books, Add redirects for deployment
    Co-Authored-By: rayhaynes504 <93682733+rayhaynes504@users.noreply.github.com>

---
+++ src/components/Booklist/Booklist.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Booklist(props) {
+    return (
+        <div>
+           Book list 
+        </div>
+    );
+}
+
+export default Booklist;
---
+++ src/components/NewBook/NewBook.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NewBook(props) {
+    return (
+        <div>
+            New Book
+        </div>
+    );
+}
+
+export default NewBook;
