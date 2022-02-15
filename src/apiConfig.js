66f43d63ac2319cfeaefc0dec2de433d3c20ca92
rayhaynes504 <twotone504@gmail.com>
2022-02-15 12:43:27 -0600

    Scaffold out BookDetail component

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,8 +1,8 @@
 const API_URL = {
 	google: 'https://www.googleapis.com/books/v1/volumes?q=',
 	url: 'https://frozen-scrubland-32630.herokuapp.com/',
+	local: 'http://localhost:8000/',
 };
 
-// const API_URL = 'http://localhost:8000/';
 
 export default API_URL;
--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import API_URL from '../../apiConfig';
+import { useParams } from 'react-router-dom';
+import Comments from '../Comments/Comments';
 
 function BookDetail(props) {
-    return (
-        <div>
-            Book Detail
-        </div>
-    );
+	const { id } = useParams();
+	const [bookData, setBookData] = useState(null);
+	useEffect(() => {
+		getSpecificBook();
+	}, []);
+
+	// Call to our API to get detailed book info
+	async function getSpecificBook() {
+		try {
+			const res = await fetch(`${API_URL.url}books/${id}`);
+			const data = await res.json();
+			setBookData(data);
+		} catch (error) {
+			// console.log(error)
+		}
+	}
+
+	if (!bookData) return null;
+
+	return (
+		<>
+			<h2>{bookData.title}</h2>
+			<p>by: {bookData.author}</p>
+			<div className='book-detail-container'>
+				<div className='book-detail'>
+					<img src={bookData.image} alt={bookData.title} />
+					<p>{bookData.category}</p>
+					<p>{bookData.description}</p>
+				</div>
+				<p>This conversation started by: {bookData.owner}</p>
+				<Comments comments={bookData.comments} />
+			</div>
+		</>
+	);
 }
 
 export default BookDetail;
