132d62a4b460d438d26b3ccb64e8953cfc39881a
rayhaynes504 <twotone504@gmail.com>
2022-02-16 11:54:03 -0600

    Add about modal

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import Modal from 'react-bootstrap/Modal';
 
-function About(props) {
-    return (
-        <div>
-            About
-        </div>
-    );
+function About({ handleClose, show }) {
+	return (
+		<>
+			<Modal show={show} onHide={handleClose}>
+				<Modal.Header closeButton>
+					<Modal.Title> Welcome to Book Talk!</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					Book Talk is a virtual book club. Search and add a new book to start a
+					new conversation or join in an ongoing conversation. Enjoy!
+				</Modal.Body>
+			</Modal>
+		</>
+	);
 }
 
 export default About;
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import About from '../About/About';
 
 function Navigation(props) {
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to='/'>Home</Link>
-                </li>
-                <li>
-                    <Link to='/about'>About</Link>
-                </li>
-                <li>
-                    <Link to='/new-book'>Add Book</Link>
-                </li>
-                <li>
-                    <Link to='login'>Login/Signup</Link>
-                </li>
-            </ul>
-        </nav>
-    );
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
+	return (
+		<nav>
+			<ul>
+				<li>
+					<Link to='/'>Home</Link>
+				</li>
+				<li>
+					<Link to='/new-book'>Add Book</Link>
+				</li>
+				<li>
+				<li onClick={handleShow}>About</li>
+					<Link to='login'>Login/Signup</Link>
+				</li>
+			</ul>
+			{show && (
+				<About handleClose={handleClose} handleShow={handleShow} show={show} />
+			)}
+		</nav>
+	);
 }
 
 export default Navigation;
