1c435440eb74f9d5ef395b425690739cc85d12ba
kgittens89 <93796800+kgittens89@users.noreply.github.com>
2022-02-15 22:06:24 -0600

    Add fetch for google books api, Display search results from api on page.

--- src/components/NewBook/NewBook.jsx
+++ src/components/NewBook/NewBook.jsx
@@ -1,36 +1,66 @@
-import React from 'react';
-import Form from 'react-bootstrap/Form';
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import Button from 'react-bootstrap/Button'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import API_URL from '../../apiConfig';
+import NewBookForm from '../NewBookForm/NewBookForm';
 
 function NewBook(props) {
+    const [formState, setFormState] = useState({
+        title: '',
+        author: '',
+        genre: '',
+        topic: ''
+    })
+    const [searchResults, setSearchResults] = useState();
     
-    const handleChange = () => {
-        console.log('hi')
+    const handleChange = (e) => {
+        setFormState({...formState, [e.target.id]: e.target.value})
     }
     
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('submit')
+        getSearchResults()
+    }
+
+    const getSearchResults = async () => {
+        // Url is interpolated, checking each of the formState keys for a value. If present the formState value is used, if not, an empty string. 
+
+        let url = `${API_URL.google}${(formState.topic ? formState.topic.replaceAll(' ', '-') : '')}${(formState.author ? API_URL.author + formState.author.replaceAll(' ', '-') : '')}${(formState.title ? API_URL.title + formState.title.replaceAll(' ', '-') : '')}${(formState.genre ? API_URL.genre + formState.genre.replaceAll(' ', '-') : '')}&key=${process.env.REACT_APP_API_KEY}`
+
+        try {
+            // const res = await fetch(`${API_URL.google}${formState.topic}+inauthor:${formState.author}+intitle:${formState.title}+subject:${formState.genre}&key=${process.env.REACT_APP_API_KEY}`)
+
+            const res = await fetch(url)
+            const data = await res.json()
+            console.log(data)
+            setSearchResults(data.items)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const handleClick = () => {
+        console.log('hi')
     }
     
     return (
         <div className='m-5 w-75 '>
-        <Form onSubmit={handleSubmit}>
-        <FloatingLabel
-        controlId="floatingTitle"
-        label="Title"
-        className="mb-3"
-        >
-        <Form.Control placeholder="Title" onChange={ handleChange} />
-        </FloatingLabel>
-        <FloatingLabel controlId="floatingAuthor" label="Author">
-        <Form.Control placeholder="Author" />
-        </FloatingLabel>
-        <Button variant="secondary" type="submit" className='m-3'>
-        Submit
-        </Button>
-        </Form>
+            <NewBookForm
+                formState={formState}
+                handleChange={handleChange}
+                handleSubmit={handleSubmit}
+            />
+            
+            {searchResults ? searchResults.map((result) => {
+                return (
+                    <div key={result.id} onClick={handleClick}>
+                        {/* <Link to={`/book/${result}`} > */}
+                        <p>{result.volumeInfo.title}</p>
+                            <img src={result.volumeInfo.imageLinks.thumbnail} alt={result.volumeInfo.title} />
+                            {/* </Link> */}
+                    </div>
+                )
+            }) :
+            ''}
         </div>
         );
     }
