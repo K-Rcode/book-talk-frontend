de26166f1a6ae5a1ccd9c49dbc2e0095173d58a9
kgittens89 <93796800+kgittens89@users.noreply.github.com>
2022-02-15 22:04:47 -0600

    Add form to add new book using bootstrap, add variables for google api fetch to apiConfig file

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -2,6 +2,9 @@ const API_URL = {
 	google: 'https://www.googleapis.com/books/v1/volumes?q=',
 	url: 'https://frozen-scrubland-32630.herokuapp.com/',
 	local: 'http://localhost:8000/',
+	author: '+inauthor:',
+	title: '+intitle:',
+	genre: '+insubject:',
 };
 
 
