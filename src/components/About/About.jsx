3f74955dcc22925ad88ce474164a2a88ce270054
kgittens89 <93796800+kgittens89@users.noreply.github.com>
2022-03-09 21:12:14 -0600

    Add developer images with linkedin hyperlink to about modal

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,5 @@
-import Modal from 'react-bootstrap/Modal';
+import {Modal, Stack} from 'react-bootstrap';
+
 
 function About({ handleClose, show }) {
 	return (
@@ -11,6 +12,37 @@ function About({ handleClose, show }) {
 					Book Talk is a virtual book club. Search and add a new book to start a
 					new conversation or join in an ongoing conversation. Enjoy!
 				</Modal.Body>
+				<h3 style={{textAlign: 'center'}}>Development Team</h3>
+				<Stack direction="horizontal" gap={3} className='about-img'>
+				<div>
+						<a
+							className='linkedIn'
+							target='_blank'
+							rel='noreferrer'
+							href='https://www.linkedin.com/in/ray-haynes/'>
+							<img
+								className='headshotImg'
+								src='https://ca.slack-edge.com/T0351JZQ0-U02LLLN9U58-b2a439a24029-192'
+								alt='Ray'
+							/>
+						</a>
+						<h4>Ray Haynes</h4>
+				</div>
+				<div>
+						<a
+							className='linkedIn'
+							target='_blank'
+							rel='noreferrer'
+							href='https://www.linkedin.com/in/keisha-gittens/'>
+							<img
+								className='headshotImg'
+								src='https://media-exp1.licdn.com/dms/image/C4D03AQE3qA2WbP184w/profile-displayphoto-shrink_400_400/0/1639697633507?e=1651708800&v=beta&t=E3GDKooMmEVuitMTFog7hu2O_ZrZUUmyhi9eIBW5obw'
+								alt='Keisha'
+							/>
+						</a>
+						<h4>Keisha Gittens</h4>
+					</div>
+					</Stack>
 			</Modal>
 		</>
 	);
