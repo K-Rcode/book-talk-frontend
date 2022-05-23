11020c9bd93b3de1d7b5d234dd2329fa2cf5f540
kgittens89 <93796800+kgittens89@users.noreply.github.com>
2022-05-22 20:35:38 -0500

    Add collapsible for book summary, minor style updates

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react';
 import API_URL from '../../apiConfig';
 import { useParams } from 'react-router-dom';
 import Comments from '../Comments/Comments';
-import { CardActionArea, Card, CardContent, CardMedia, Typography, Container, } from '@mui/material';
+import { CardActionArea, Card, CardContent, CardMedia, Typography, Container, Collapse, Divider } from '@mui/material';
+import { MdUnfoldMore } from 'react-icons/md';
 
 function BookDetail({ logInStatus, userData }) {
 	const { id } = useParams();
 	const [bookData, setBookData] = useState(null);
+	const [expanded, setExpanded] = useState(false)
 	useEffect(() => {
 		getSpecificBook();
 		return () => {
@@ -15,7 +17,6 @@ function BookDetail({ logInStatus, userData }) {
 		//eslint-disable-next-line
 	}, [id]);
 
-	// Call to our API to get detailed book info
 	async function getSpecificBook() {
 		try {
 			const res = await fetch(`${API_URL.url}books/${id}`);
@@ -26,11 +27,19 @@ function BookDetail({ logInStatus, userData }) {
 		}
 	}
 
+	const handleClick = () => {
+		setExpanded(!expanded)
+	}
+
 	if (!bookData) return null;
 
 	return (
 		<>
-			<Container maxWidth="md" sx={{ display: 'flex'}}>
+			<Typography gutterBottom variant='h4' 	component='div'>
+					{bookData.title}
+			</Typography>
+			<Typography variant='body1'>by: {bookData.author}</Typography>
+			<Container maxWidth="md" sx={{ display: 'flex', marginTop: '4%'}}>
 				<Card
 					sx={{ maxWidth: '70%', border: 2, borderColor: 'grey.500', }}
 					className='m-auto'>
@@ -40,15 +49,16 @@ function BookDetail({ logInStatus, userData }) {
 							height='30%'
 							image={bookData.image}
 							alt={bookData.title}
+							sx={{height: '350px', objectFit: 'contain'}}
 						/>
+						<Divider />
 						<CardContent>
-							<Typography gutterBottom variant='h5' component='div'>
-								{bookData.title}
-							</Typography>
-							<Typography variant='body1'>by: {bookData.author}</Typography>
+							Book summary <MdUnfoldMore onClick={handleClick } />
+							<Collapse in={expanded} timeout="auto" unmountOnExit>
 							<Typography variant='body2' color='text.secondary'>
 								{bookData.description}
-							</Typography>
+								</Typography>
+								</Collapse>
 						</CardContent>
 					</CardActionArea>
 				</Card>
